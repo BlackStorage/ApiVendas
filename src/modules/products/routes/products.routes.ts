b3545af8735213efd242dccea0314c9a6d5f53a1
Gabriel Fernandes <blackstorage.brazil@gmail.com>
2022-10-26 08:05:01 -0300

    subindo para conta correta

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -8,6 +8,6 @@ productsRouter.get('/', productsController.index);
 productsRouter.get('/:id', productsController.show);
 productsRouter.post('/', productsController.create);
 productsRouter.put('/:id', productsController.update);
-productsController.delete('/:id', productsController.delete);
+/* productsController.delete('/:id', productsController.delete); */
 
 export default productsRouter;
