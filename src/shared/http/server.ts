2c1b41e21262be685c80d04a669451bf3b249dbd
Gabriel Fernandes <blackstorage.brazil@gmail.com>
2022-10-28 08:27:44 -0300

    update index.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,39 +1,33 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
+import cors from 'express';
 import routes from './routes';
 import AppError from './erros/AppError';
 import '@shared/typeorm';
 
 const app = express(); // atribuição do xpress para uma variável;
-
-app.use(cors()); // para aceitar requisições de qualquer um;
+app.use(cors());
+// para aceitar requisições de qualquer um;
 app.use(express.json()); // para poder interpretar arquivos .json;
 
 app.use(routes); // habilitação das rotas, no arquivo index.ts;
 
-// middle, usado para tratamento de erros;
 app.use(
-  (
-    error: Error,
-    request: Request,
-    response: Response,
-    next: NextFunction
-  ) => {
+  // middle, usado para tratamento de erros;
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
     if (error instanceof AppError) {
       return response.status(error.statusCode).json({
         status: 'error',
-        message: error.message
+        message: error.message,
       });
     }
-      return response.status(500).json({
-        status: 'error',
-        message: 'Inernal servor error'
+    return response.status(500).json({
+      status: 'error',
+      message: 'Inernal servor error',
     });
   },
-)
+);
 const porta = 3333;
 app.listen(porta, () => {
   console.log('Server started on port: ', porta);
 });
-
