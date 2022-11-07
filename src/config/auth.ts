58e54d656701470a65cac8d183ba6346b182421f
Gabriel Fernandes <blackstorage.brazil@gmail.com>
2022-11-07 07:58:46 -0300

    first add of day

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'lLKJHi76yfJHU65hujJYt786t54654687/LKu',
+    expiresIn: '1d',
+  },
+};
--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,38 +1,42 @@
-/* import AppError from '@shared/http/erros/AppError';
-import { hash } from 'bcryptjs';
+import AppError from '@shared/http/erros/AppError';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getCustomRepository } from 'typeorm';
-import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 
 interface IRequest {
   email: string;
   password: string;
-  token: undefined;
 }
 
 interface IResponse {
-  user: User;
+  email: string;
+  token: string;
 }
-
-class CreateUserService {
+class CreateSessionsService {
   public async execute({ email, password }: IRequest): Promise<IResponse> {
     const userRepository = getCustomRepository(UsersRepository);
-    const emailExists = await userRepository.findByEmail(email);
+    const user = await userRepository.findByEmail(email);
 
-    if (emailExists) {
-      throw new AppError('Email address alredy used!');
+    if (!user) {
+      throw new AppError('Incorrect email/password conbination.', 401);
     }
 
-    const hashedPassword = await hash(password, 8);
+    const passwordConfirmed = await compare(password, user.password);
 
-    const user = userRepository.create({
-      email,
-      password: hashedPassword,
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect email/password conbination.', 401);
+    }
+
+    const token = sign({}, 'lLKJHi76yfJHU65hujJYt786t54654687/LKu', {
+      subject: user.id,
+      expiresIn: '1d',
     });
 
-    await userRepository.save(user);
-
-    return user;
+    return {
+      email,
+      token,
+    };
   }
 }
-export default CreateUserService; */
+export default CreateSessionsService;
